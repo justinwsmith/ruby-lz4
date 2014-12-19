d315706d64a7ab0dc3ef6793b1d70062ea77aeab
Justin Smith <justin.smith@ge.com>
2014-12-18 23:13:53 -0500

    more testing for RollingChecksumReader

--- spec/rolling_checksum_reader_spec.rb
+++ spec/rolling_checksum_reader_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 require 'stringio'
 require 'rolling_checksum_reader'
 
-$hashes = []
+$normal_hashes = []
+$short_hashes = []
 
 describe RollingChecksumReader do
 
@@ -17,7 +18,7 @@ describe RollingChecksumReader do
       @input.length.times do |i|
         byte = @whash.getbyte do |pos, hash|
           expect(pos).to eq(i)
-          $hashes << hash
+          $normal_hashes << hash
         end
         expect(byte).to eq(@input[i].ord)
       end
@@ -26,40 +27,36 @@ describe RollingChecksumReader do
     it "getbyte should return a hash for each byte" do
       result = @whash.read(@input.length) do |pos, hashes|
         expect(pos).to eq(0)
-        expect(hashes).to eq($hashes)
+        expect(hashes).to eq($normal_hashes)
       end
       expect(result).to eq(@input)
     end
 
-=begin
-    it "should hash using seed" do
-      ary = (2*@whash.max_size).times.map { rand(256) }
-      (0...@whash.max_size).each { |i| @whash.add_byte(ary[i]) }
+  end
 
+  describe 'short input' do
+    before do
+      @input = 'ac'
+      @whash = RollingChecksumReader.new StringIO.new(@input.dup)
+    end
 
-      (0...@whash.max_size).each do |i|
-        result = @whash.hash
-        hash = (i...(i+@whash.max_size)).map {|j| (@whash.seed ** (@whash.max_size-(j-i)-1)) * ary[j] }.inject(:+)
-        expect(result).to eq(hash)
-        index = i + @whash.max_size
-        @whash.add_byte(ary[index])
+    it "getbyte should return a hash for each byte" do
+      @input.length.times do |i|
+        byte = @whash.getbyte do |pos, hash|
+          expect(pos).to eq(i)
+          $short_hashes << hash
+        end
+        expect(byte).to eq(@input[i].ord)
       end
     end
 
-    it "should hash using seed" do
-      ary = (2*@whash.max_size).times.map { rand(256) }
-      (0...@whash.max_size).each { |i| @whash.add_byte(ary[i]) }
-
-
-      (0...@whash.max_size).each do |i|
-        result = @whash.hash
-        hash = (i...(i+@whash.max_size)).map {|j| (@whash.seed ** (@whash.max_size-(j-i)-1)) * ary[j] }.inject(:+)
-        expect(result).to eq(hash)
-        index = i + @whash.max_size
-        @whash.add_byte(ary[index])
+    it "getbyte should return a hash for each byte" do
+      result = @whash.read(@input.length) do |pos, hashes|
+        expect(pos).to eq(0)
+        expect(hashes).to eq($short_hashes)
       end
+      expect(result).to eq(@input)
     end
-=end
 
   end
 
